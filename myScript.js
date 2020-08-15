68951eb0b96298f65e521389d5609cd56f024769
Tanvir <tanvirrahman0047@gmail.com>
2020-08-15 12:37:02 +0600

    updated1

--- myScript.js
+++ myScript.js
@@ -5,14 +5,14 @@ var temp = document.querySelector('.temp')
 var lead = document.querySelector('.lead')
 
 button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&appid=f2adf456f891e7a9617f32903727cf9f')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f2adf456f891e7a9617f32903727cf9f')
 .then(response => response.json())
 .then(data => {
     var nameValue = data['name'];
+    console.log(nameValue,name)
     var tempValue = data['main']['temp'];
     var leadValue = data['weather'][0]['description'];
-
-    name.innerHTML = nameValue;
+    document.querySelector('.name').innerHTML = nameValue;
     temp.innerHTML = tempValue;
     lead.innerHTML = leadValue;
 })
